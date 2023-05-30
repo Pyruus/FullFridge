9754b111b33ebb858e0401c1b92f5cdcf06629bb
Kamil Naglik <k.naglik@donedeliveries.com>
2023-05-30 19:38:45 +0200

    [FE] register logic

--- FullFridge.UI/src/app/register/register.component.ts
+++ FullFridge.UI/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+  readonly ROOT_URL = 'https://localhost:7040/api'
+  
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) 
+  {
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+   }
 
   ngOnInit(): void {
   }
 
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+  
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      if (confirmPassword.errors) {
+        confirmPassword.setErrors({ ...confirmPassword.errors, 'passwordMismatch': true });
+      } else {
+        confirmPassword.setErrors({ 'passwordMismatch': true });
+      }
+    } else {
+      if (confirmPassword) {
+        confirmPassword.setErrors(null);
+      }
+    }
+  }
+
+  submitForm(): void {
+
+    if (this.myForm.valid) {
+      const formData = this.myForm.value;
+
+      const requestBody = {
+        email: formData.email,
+        password: formData.password,
+        details: {
+          name: formData.name,
+          surname: formData.surname
+        }
+      };
+
+      this.http.post(this.ROOT_URL + `/User/Register`, requestBody).subscribe(
+        (response: any) => {
+          console.log(requestBody); // Print the fetched data
+        },
+        (error: any) => {
+          console.error(error);
+        }
+    );
+    }
+  }
 }
