f1fe6fd6ed7dddaceb8f1ac51ac62670918826d6
Pyrus <pyruuss@gmail.com>
2023-06-03 18:15:01 +0200

    [FE] commenting on recipes

--- FullFridge.UI/src/app/recipe/recipe.component.ts
+++ FullFridge.UI/src/app/recipe/recipe.component.ts
@@ -4,8 +4,10 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { FileUploadService } from '../file-upload.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -14,22 +16,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./recipe.component.css'],
 })
 export class RecipeComponent implements OnInit {
-
   recipeId: any;
   readonly ROOT_URL = 'https://localhost:7040/api'
   recipe: any;
   fileUrl: string | null = null;
   sanitizedFileUrl: SafeUrl | null = null;
   faTrash = faTrash;
+  faLike = faThumbsUp;
+  faDislike = faThumbsDown;
+  myForm: FormGroup;
+  isLike = true;
 
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private fileService: FileUploadService,
-    private sanitizer: DomSanitizer,
-    private cookieService: CookieService,
-    private router: Router
-  ) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private fileService: FileUploadService, private sanitizer: DomSanitizer, private cookieService: CookieService, private router: Router, private formBuilder: FormBuilder, private location: Location) {
+    this.myForm = this.formBuilder.group({
+      commentContent: ['', Validators.required],
+    });
+  }
 
 
   ngOnInit(): void {
@@ -75,5 +77,42 @@ export class RecipeComponent implements OnInit {
         error: error => console.error('Error:', error)
       }
     );
+  }
+
+  submitForm(): void {
+
+    if (this.myForm.valid) {
+      const formData = this.myForm.value;
+
+      const token = this.cookieService.get("token");
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+
+      const requestBody = {
+        content: formData.commentContent,
+        createdById: this.cookieService.get("userId"),
+        isLike: this.isLike,
+        recipeId: this.recipe.id
+      };
+
+      this.http.post(this.ROOT_URL + `/Recipe/Comment`, requestBody, { headers }).subscribe(
+        {
+          next: response => {
+            console.log(response);
+            window.location.reload();
+          },
+          error: error => console.error(error)
+        }
+      );
+    }
+  }
+
+  like(){
+    this.isLike = true;
+  }
+  
+  dislike(){
+    this.isLike = false;
   }
 }
